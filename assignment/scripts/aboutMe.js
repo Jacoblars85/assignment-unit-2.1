541e0b9847da67916959c63ec9a80c5a15ec2ee8
Jacob Larson <hockeybobber@comcast.net>
2023-09-28 14:12:17 -0500

    just finished the variables part of the assignment

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -43,20 +43,36 @@ let friendsPets = 1
 
 console.log(friendsPets);
 // 10 - Add two pets to your `pets` variable
+pets + 2
 
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+const allowedPets = 11
 
 // 12 - Create a variable called `result`. Create a conditional:
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
+let result;
 
+if (adventurous === true) {
+  result = 'Adventures are great!';
+} else {
+  result = 'How about we stay home?';
+}
+
+console.log(result);
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
 //      if luckyNumber is 2 and adventurous is true,
 //      set `diceRoll` to be "Roll the dice!"
 //      Console log the value of `diceRoll`
+let diceRoll = 'Try again later.'
 
+if (luckyNumber === 2 && adventurous === true) {
+  diceRoll = 'Roll the dice!';
+} 
+
+console.log(diceRoll);
 // 14 - Create a variable called `petStatus`. 
 //      Write a conditional that covers the following:
 //      if the value of `pets` is less than the value of `allowedPets`,
@@ -65,7 +81,17 @@ console.log(friendsPets);
 //      set `petStatus` to the value of "I have enough pets" 
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
+let petStatus;
 
+if (pets < allowedPets) {
+  petStatus = 'I can have more pets'
+} else if (pets === allowedPets) {
+  petStatus = 'I have enough pets'
+} else {
+  petStatus = 'Oh no, I have too many pets!'
+}
+  
+ console.log(petStatus);
 
 // STRETCH GOALS:
 
